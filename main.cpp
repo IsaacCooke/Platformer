9986810fa4c88c782001f6aa8f7591c5e62d0b61
IsaacCooke <cookeisaac2@gmail.com>
2024-07-09 18:02:12 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <raylib.h>
+
+int main() {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "Platformer Game");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()){
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+g++ main.cpp -lraylib -o program
+./program
