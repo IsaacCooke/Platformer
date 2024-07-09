d68bf2205c9d471ebef853cdaf715ef6d0c93a64
IsaacCooke <cookeisaac2@gmail.com>
2024-07-09 19:03:09 +0100

    Started on player animations

--- src/characters/player.h
+++ src/characters/player.h
@@ -2,13 +2,30 @@
 
 class Player {
 private:
+  // Movement
   float speed = 2.0;
   Vector2 position;
   float movement;
+
+  // Animation
+  Texture2D idleTexture;
+  Rectangle idleFrameRec;
+  int currentFrame;
 
 public:
   // Game loop
   void init(const int screenHeight, const int screenWidth);
   void update();
   void draw();
+
+  Player(){
+    this->idleTexture = LoadTexture("../assets/characters/knight/_Idle.png");
+    this->idleFrameRec = { 0.0f, 0.0f, (float)idleTexture.width / 10, (float)idleTexture.height };
+
+    this->currentFrame = 0;
+  }
+
+  ~Player(){
+    UnloadTexture(this->idleTexture);
+  }
 };
